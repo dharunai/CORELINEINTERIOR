ae98481ec081045136863bdc6a11028d28a2da6a
PRO ACCOUNTS11 <PROACCOUNTS11@hwh.residency.hosp>
2026-06-02 18:21:24 +0530

    Make project Vercel host friendly

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,15 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import netlifyPlugin from "@netlify/vite-plugin-tanstack-start";
 
 export default defineConfig({
   plugins: [
     tanstackStart({
       server: { 
-        preset: "netlify",
+        preset: "vercel",
         entry: "src/server.ts" 
       },
     }),
-    netlifyPlugin(),
     react(),
     tailwindcss(),
     tsconfigPaths(),
